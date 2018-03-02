8263b5b5e69515ff5fcabb9e5c6b94b0ba98b23e
Piotr Kowalski <kowalskii1@op.pl>
2018-03-02 21:12:09 +0100

    add persons view and controller

---
+++ GunPermAnalysis/Controllers/GunsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GunPermAnalysis.Controllers
+{
+    public class GunsController : Controller
+    {
+        // GET: Guns
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GunPermAnalysis/Controllers/PersonsController.cs
@@ -0,0 +1,24 @@
+﻿using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GunPermAnalysis.Controllers
+{
+    public class PersonsController : Controller
+    {
+        // GET: Persons
+        public ActionResult Index()
+        {
+            List<Persons> persons = new List<Persons>();
+            persons.Add(new Persons() { Id = 1, FirstName = "Jacek", Surname = "K", City = "Lodz", AgeGroupId = 1, JobId = 1, Gender = "Male" });
+            persons.Add(new Persons() { Id = 1, FirstName = "Pawel", Surname = "K", City = "Lodz", AgeGroupId = 1, JobId = 1, Gender = "Male" });
+            persons.Add(new Persons() { Id = 1, FirstName = "Gawel", Surname = "K", City = "Lodz", AgeGroupId = 1, JobId = 1, Gender = "Male" });
+            persons.Add(new Persons() { Id = 1, FirstName = "Piotr", Surname = "K", City = "Lodz", AgeGroupId = 1, JobId = 1, Gender = "Male" });
+
+            return View(persons);
+        }
+    }
+}
